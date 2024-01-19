268d78648d39a9e500a06f10bd18a14912f54e40
kizi <phg980513@naver.com>
2024-01-19 14:10:09 +0900

    [추가] 컬러 폰트 관련 extension 추가

---
+++ SeSAC_Recap/Extension/UIColor+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Extension.swift
+//  SeSAC_Recap
+//
+//  Created by 박희지 on 1/19/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static var point: UIColor {
+        return UIColor(red: 73, green: 220, blue: 146, alpha: 1)
+    }
+    
+    static var background: UIColor {
+        return .black
+    }
+    
+    static var text: UIColor {
+        return .white
+    }
+}
---
+++ UIFont+Extension.swift
@@ -0,0 +1,50 @@
+//
+//  UIFont+Extension.swift
+//  SeSAC_Recap
+//
+//  Created by 박희지 on 1/19/24.
+//
+
+import UIKit
+
+extension UIFont {
+    static var sf13 : UIFont {
+        return .systemFont(ofSize: 13, weight: .regular)
+    }
+    
+    static var sf13Bold : UIFont {
+        return .systemFont(ofSize: 13, weight: .bold)
+    }
+    
+    static var sf14 : UIFont {
+        return .systemFont(ofSize: 14, weight: .regular)
+    }
+    
+    static var sf14Bold : UIFont {
+        return .systemFont(ofSize: 14, weight: .bold)
+    }
+    
+    static var sf15 : UIFont {
+        return .systemFont(ofSize: 15, weight: .regular)
+    }
+    
+    static var sf15Bold : UIFont {
+        return .systemFont(ofSize: 15, weight: .bold)
+    }
+    
+    static var sf16 : UIFont {
+        return .systemFont(ofSize: 16, weight: .regular)
+    }
+    
+    static var sf16Bold : UIFont {
+        return .systemFont(ofSize: 16, weight: .bold)
+    }
+    
+    static var sf17 : UIFont {
+        return .systemFont(ofSize: 17, weight: .regular)
+    }
+    
+    static var sf17Bold : UIFont {
+        return .systemFont(ofSize: 17, weight: .bold)
+    }
+}
