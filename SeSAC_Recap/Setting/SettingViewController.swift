b1ec31aa7f38d3cc1744075c4c347ad22a638835
kizi <phg980513@naver.com>
2024-01-22 15:41:54 +0900

    [추가] #4: 탭바 설정

---
+++ SeSAC_Recap/Setting/SettingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingViewController.swift
+//  SeSAC_Recap
+//
+//  Created by 박희지 on 1/22/24.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "설정"
+    }
+
+}
